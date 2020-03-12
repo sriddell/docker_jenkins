aae702de823f2748c694508ec966d5815ef7a3b5
Shane Riddell <shane.riddell@ellucian.com>
2020-03-12 12:37:29 +0000

    Fix known_hosts

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 #Install nvm
 sudo chown jenkins:jenkins /var/jenkins_home/workspace
+rm -f /var/jenkins_home/.ssh/known_hosts # reset known hosts in case gitlab, etc location has changed
 set -e
 echo "installing plugins"
 /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
