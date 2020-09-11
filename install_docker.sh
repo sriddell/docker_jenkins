56584d405b1ae880a12ee3e3f3d5e15435f0f688
Shane Riddell <shane.riddell@ellucian.com>
2020-09-11 15:32:32 +0000

    Fix install docker

--- install_docker.sh
+++ install_docker.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set +x
+set -e
 cd /tmp
 if ! [ ${CI_DOCKER_VERSION} ]; then
-    CI_DOCKER_VERSION=latest
+    CI_DOCKER_VERSION=18.09.9
 fi
 echo "installing docker client binary version $CI_DOCKER_VERSION"
-#curl -L https://get.docker.com/builds/Linux/x86_64/docker-$CI_DOCKER_VERSION.tgz -o docker.tgz
-curl -L https://get.docker.com/builds/Linux/x86_64/docker-17.04.0-ce.tgz -o docker.tgz
+curl -L https://download.docker.com/linux/static/stable/x86_64/docker-$CI_DOCKER_VERSION.tgz -o docker.tgz
+#curl -L https://get.docker.com/builds/Linux/x86_64/docker-17.04.0-ce.tgz -o docker.tgz
 mkdir extract
 cd extract
 tar xzf ../docker.tgz
