59ecc13fe7dda12ba82fe33db5283a2526ecfb5c
Shane Riddell <shane.riddell@ellucian.com>
2021-09-10 10:56:52 +0000

    Fix gitlab_init

--- gitlab_init.rb
+++ gitlab_init.rb
@@ -1,4 +1,14 @@
 u = User.find_by_username('root');
-token = u.personal_access_tokens.create(scopes: [:api], name: 'jenkins');
-token.set_token("skfj2348yrhauewsdfisa");
-token.save!
+tokens = u.personal_access_tokens
+found = false
+tokens.each do |t|
+    if t.name == 'jenkins'
+        found = true
+        break
+    end
+end
+if !found
+    token = u.personal_access_tokens.create(scopes: [:api], name: 'jenkins');
+    token.set_token("skfj2348yrhauewsdfisa");
+    token.save!
+end
