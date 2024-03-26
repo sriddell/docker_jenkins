eb83a1790210097af62d5e8dc01447e21fd6df3d
Shane Riddell <shane.riddell@ellucian.com>
2024-03-26 10:13:17 -0400

    Move to docker in docker

--- start-node.sh
+++ start-node.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo service ssh restart
+sudo start-docker.sh
 java -jar /home/jenkins/agent.jar -jnlpUrl "http://jenkins:8080/computer/$WORKER_NAME/jenkins-agent.jnlp" -workDir /home/jenkins/workspace -failIfWorkDirIsMissing
